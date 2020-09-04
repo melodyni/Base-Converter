6e7b6d87177ce36baef6aa5fb3312396739edd12
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-09-04 19:23:57 +0530

    converted BaseConverter class component into functional component, added useState hook

--- src/BaseConverter.js
+++ src/BaseConverter.js
@@ -1,29 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputBox from './InputBox.js';
 import './baseConverter.css';
 
-class BaseConverter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: 0 };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(value) {
-    this.setState({ value: value });
-  }
-
-  render() {
-    const inputBoxes = this.props.bases.map((base) => (
-      <InputBox
-        base={base}
-        key={base}
-        onChange={this.handleChange}
-        value={this.state.value}
-      />
-    ));
-    return <div className='flex'>{inputBoxes}</div>;
-  }
-}
+const BaseConverter = (props) => {
+  const [state, setValue] = useState({ value: 0 });
+  const inputBoxes = props.bases.map((base) => (
+    <InputBox
+      base={base}
+      key={base}
+      onChange={(value) => setValue({ value })}
+      value={state.value}
+    />
+  ));
+  return <div className='flex'>{inputBoxes}</div>;
+};
 
 export default BaseConverter;
